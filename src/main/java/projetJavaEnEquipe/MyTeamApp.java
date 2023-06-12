24e78fe09892ee73f098fd60ae3807d2ec0ee1aa
fredericRos <frederic.ros@gmail.com>
2023-06-12 15:04:07 +0200

    initial projet de groupe

---
+++ src/main/java/projetJavaEnEquipe/MyTeamApp.java
@@ -0,0 +1,10 @@
+package projetJavaEnEquipe;
+
+public class MyTeamApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Notre premier projet de groupe 3 : lundi 12 juin 2023 15h00");
+	}
+
+}
